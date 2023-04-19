bfb0479033633781774fcbb29af1280273142c29
vmewada01 <vishalmewada9826@gmail.com>
2023-04-19 20:59:56 +0530

    road_to_React

---
+++ Advance Javascript/Road To React 1/components/navbar.js
@@ -0,0 +1,23 @@
+function navbar() {
+    return `
+    <h3>
+    <a href="index.html">HOME</a>
+</h3>
+<div id="options">
+    <h3>
+        <a href="./jewellery.html">JEWELLERY</a>
+    </h3>
+    <h3>
+        <a href="electronic.html">ELECTORNICS</a>
+    </h3>
+    <h3>
+        <a href="login.html">LOGIN</a>
+
+    </h3>
+    <h3>
+        <a href="signup.html">SIGN UP</a>
+    </h3>
+    
+    `
+}
+export {navbar};
---
+++ Advance Javascript/Road To React 1/src/index.js
@@ -0,0 +1,9 @@
+import {navbar} from "../components/navbar";
+
+import "../styles/navbar.css";
+
+// console.log(navbar);
+
+let navbar_div = document.getElementById("navbar")
+
+navbar_div.innerHTML=navbar();
