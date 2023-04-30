e52f0dfaf39f52f4bcceb8717956ea6a15505332
vmewada01 <vishalmewada9826@gmail.com>
2023-04-30 20:40:33 +0530

    adding

--- REACT/React_Styles/styles/src/App.js
+++ REACT/React_Styles/styles/src/App.js
@@ -1,18 +1,13 @@
-import logo from './logo.svg';
+import SimpleSidebar from "./components/Siderbar";
 
-import {AppStyled, AppHeader, AppLogo,Flexbox} from "./App.styled"
 
 function App() {
   return (
-    <AppStyled width="10px" name="Vishal">
-      <AppHeader>
-        <AppLogo src={logo} alt="logo" />
-       <Flexbox flexDirection="column">
-      <div>A</div>
-      <div>B</div>
-      </Flexbox>
-      </AppHeader>
-    </AppStyled>
+<div>
+
+<SimpleSidebar/>
+
+</div>
   );
 }
 
